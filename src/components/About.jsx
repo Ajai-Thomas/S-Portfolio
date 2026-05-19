b2a6323e560f51ed0c36f5947415a64f34639001
Ajai Thomas <ajaiabraham2005@gmail.com>
2026-05-19 15:50:06 +0530

    updated Name

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className="pt-32 pb-24 grid grid-cols-1 md:grid-cols-2 gap-x-24 gap-y-12 items-start">
       <div className="font-sans">
         <h3 className="text-xs font-bold tracking-[0.2em] mb-6 uppercase text-black/40">Introduce Yourself</h3>
-        <p className="text-2xl font-bold mb-4 font-display">Agnieszka Trefler</p>
+        <p className="text-2xl font-bold mb-4 font-display">Sreerag</p>
         <p className="text-base leading-relaxed text-black/70 max-w-sm">
           My work is known from community festivals like Audioriver or Up To Date Festival. I professionally deal with photography and creating creative videos.
         </p>
